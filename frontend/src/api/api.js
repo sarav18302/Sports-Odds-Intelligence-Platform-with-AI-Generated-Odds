380c3856555fd5d6ba9e7905e0eb2c064f455f38
sarav18302 <sarav18302@gmail.com>
2026-04-08 01:56:49 +0530

    Add React URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export async function fetchMatches() {
   const res = await fetch(`${BASE_URL}/matches`);
